d8dab591038e91e59154a7cbbeb901189442b44b
Vikyle Naidoo <harris.graemeza@gmail.com>
2019-12-03 16:41:48 +0200

    tracking using angle. sending angle to jetson @90hz. problem is the fps is not consistent (89-91Hz)

--- main.py
+++ main.py
@@ -4,59 +4,50 @@ import jetson.inference
 import jetson.utils
 import Jetson.GPIO as GPIO
 import time
-from multiprocessing import Process, Pipe, Manager
+from multiprocessing import Process, Pipe
 import math
 import serial
 #import struct
-import xiCamera
-import pickle
-import cv2
-import os
 
-#TODO: ximea capture working, inference working. run benchmarks on performance(capturing, sending, inference), sort out adruino tracking code
-#bottleneck: camera capture @10fps, dumping data to disk takes a while
+
+
+#TODO: problem: arduino is updating the angle @900Hz fastest resulting in buffer getting full before it can be read by nano. thus there is a delay in angle data incoming when the motor is moving fast
 
 
 display_size = (320,240)	#(width, height) 320, 240
-manager = Manager()
-#image_data = [] # manager.list() #stores list of numpy arrays of data from camera
-#angle_data = manager.list()
-#image_timestamps = [] #manager.list()
-#angle_timestamps = manager.list()
-#camera = xiCamera.Camera()
+
 arduino = serial.Serial("/dev/ttyUSB0", baudrate=115200, write_timeout=None)
 
 def main():
 	x_center = display_size[0]/2
 	dead_center = display_size[0]/2	
+	
 
 	try:	
-		#capture camera stream
-		capture_last_image_conn, inference_last_image_conn = Pipe() 
-		process_cameraCapture = Process(target=camera_capture, args=(capture_last_image_conn,))
-		process_cameraCapture.start() 
-		time.sleep(10)
-
 		detect_xcenter_conn, main_xcenter_conn = Pipe() #xcenter will be updated in the detect thread and used in main thread
-		
+		#receive_angle_conn, main_angle_conn = Pipe() #should use a 2d array for angles and timestamps
+
 		#inference process
-		process_detection = Process(target=object_detection, args=(detect_xcenter_conn, inference_last_image_conn))	
+		process_detection = Process(target=object_detection, args=(detect_xcenter_conn,))	
 		process_detection.start()
 		
 		arduino.reset_output_buffer()
-		time.sleep(10)
+		time.sleep(15) #wait forinference to startup
+		
+		#angle receive process
+		process_angle = Process(target=receive_angle)	
+		process_angle.start()
 
 		print("-------------TRACKING STARTED---------------")	
 
 		last_time = time.time()
 
 		while(1):
-			#print("[main]\t effective fps = ", 1/(time.time()-last_time))
-			
 			
 			if(main_xcenter_conn.poll(0.01)):
-				print("[main]\t sending fps = ", 1/(time.time()-last_time))
-				last_time = time.time()
+				t1 = time.time()
+				#print("[main]\t sending fps = ", 1/(t1-last_time))
+				last_time = t1
 				x_center = main_xcenter_conn.recv()		
 					
 				if(x_center<display_size[0] and x_center>0):
@@ -65,22 +56,18 @@ def main():
 					print("[main]\t xcenter out of range")	
 
 				#print("[main]\t xcenter: {:d}".format(int(x_center)))
-			#if(arduino.in_waiting > 2):
-			#	angle_data.append(read_angle_from_arduino())
-			#	angle_timestamps.append(time.time())
-			#	print("[main]\t received angle = ", angle_data[-1])	
-			
+
+
 			
 								
 	except KeyboardInterrupt:
 		print("-----------keyboard interrupt---------------")
 		
 	finally:
-		#global camera
 		print("------------------EXITING-------------------")	
-		process_cameraCapture.join()		
-	
 		arduino.reset_output_buffer()		
+		
+		process_angle.join()		
 		arduino.close()		
 		process_detection.join()
 		
@@ -88,31 +75,23 @@ def main():
 		
 
 
-def object_detection(x_center_conn, last_image_conn):
+def object_detection(x_center_conn):
 	net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.4) #ssd-mobilenet-v2
-	#camera = jetson.utils.gstCamera(display_size[0], display_size[1], "/dev/video0") #/dev/video0
-	#display = jetson.utils.glDisplay()
+	camera = jetson.utils.gstCamera(display_size[0], display_size[1], "/dev/video0") #/dev/video0
+	display = jetson.utils.glDisplay()
 
 	last_time = time.time()
-
-	while (display.IsOpen()):
-		now = time.time()
-		print("[Detection] length of detections = ", (now-last_time))
-		last_time = now
-		#time.sleep(0.1)
-		#img, width, height = camera.CaptureRGBA(zeroCopy=True)
-		
-#		print("[detection]\t len image_data: ", len(image_data))
-		try:
-			img = cv2.resize(last_image_conn.recv(), dsize=(320, 240)) #use latest image in list
-			img = jetson.utils.cudaFromNumpy(img)
+	try:
+		while (display.IsOpen()):
+			now = time.time()
+			#print("[Detection] length of detections = ", (now-last_time))
+			last_time = now
 			
-			width = display_size[0]
-			height = display_size[1]
+			img, width, height = camera.CaptureRGBA()		
 
+			#run the detection
 			detections = net.Detect(img, width, height)
 			display.RenderOnce(img, width, height)	
-			
 			
 			#max confidence object
 			if(len(detections)==0):
@@ -134,54 +113,54 @@ def object_detection(x_center_conn, last_image_conn):
 					#net.PrintProfilerTimes();
 					fps = 1000/net.GetNetworkTime()
 					display.SetTitle("FPS: {:.2f} \t Class: {:s} \t Confidence: {:.2f} \t xcenter: {:d}".format(fps, net.GetClassDesc	(d_max.ClassID), d_max.Confidence, xcenter))
-		except IndexError:
-			print("[detection]\t Index out of bounds")
+	except KeyboardInterrupt:
+		print("-------------------keyboard interrupt detection -------------")		
 
-def camera_capture(last_image_conn):
-	print("[cam process]\t opening cam")
-	camera = xiCamera.Camera()
+
+def receive_angle():
+	print("---------------- STARTING ANGLE READ ----------------------")
+	angle_data = []
+	angle_timestamps = []
 	t0 = time.time()
-	image_data = []
-	image_timestamps = []
-	#os.nice(2)
+
 	try:
-		while(1):
-			t1 = time.time() 
-			print("[cam process]\t time between samples: ", (t1-t0))
-			t0 = t1
-			if(len(image_data)<20):
-				xi_image = camera.captureFrame()
-				image_data.append(xi_image.get_image_data_numpy())
-				last_image_conn.send(image_data[-1])
-				image_timestamps.append(time.time())
-				
+		while(1):		
+			if(arduino.in_waiting > 2):
+				angle_data.append(read_angle_from_arduino())
+				t1 = time.time()
+				angle_timestamps.append(t1-t0)
+				t0 = t1
+				print("[angle]\t received angle = ", angle_data[-1], "\tat: ", angle_timestamps[-1])	
 			else:
-				print("[cam process]\t max length reached")
-				#dump all data to disk
-				with open('image_save.pkl', 'wb') as f:
-					pickle.dump((image_data), f)
-
-				#clear lists in memory 
-				image_data[:] = []
-				image_timestamps[:] = []
-				#angle_data[:] = []
-				#angle_timestamps[:] = []
-				
-	except KeyboardInterrupt:		
-		camera.close()		
-		quit()
-
+				angle_data.append(angle_data[-1])
+				t1 = time.time()
+				angle_timestamps.append(t1-t0)
+				t0 = t1
+				print("[angle]\t received angle = ", angle_data[-1], "\tat: ", angle_timestamps[-1])	
+			time.sleep(0.01076)
+	
+	except KeyboardInterrupt:
+		print("-------------------keyboard interrupt angle -------------")		
+		print("angles = ", angle_data)		
+		print("times = ", angle_timestamps)
 
 def send_to_arduino(xcenter):
-	data = xcenter.to_bytes(2, byteorder='little', signed=False) #struct.pack('<I', xcenter)
+	data = xcenter.to_bytes(2, byteorder='big', signed=False) #struct.pack('<I', xcenter)
 	#print("[comm]\t  data: ", (data))
 	arduino.write(data)
 
 def read_angle_from_arduino():
 	data = arduino.read(2)	#highbyte then lowbyte
-	steps = int.from_bytes(data, byteorder='big', signed=False) 
+	steps = int.from_bytes(data, byteorder='little', signed=False) 
 	return 0.15*steps #using halfstepping ==> 0.15* per step
 	
+
+
+
+
+
+
+
 if __name__=='__main__':
 	main()
 
--- motorControl/motorControl.ino
+++ motorControl/motorControl.ino
@@ -3,36 +3,51 @@
 *
 *
 */
-
+//TODO: fix code for target_angle, find c
 
 #include <math.h>
-#include <TimerOne.h>
-unsigned int xcenter;
-int dir;
-short steps;
-unsigned long period;
+
+const int ZERO_PIN = 2;
 const int STEP_PIN = 10;
 const int DIR_PIN = 11;
 const int width = 320;
+
+const float a = 0.03; //0.03
+const float b = 0.005;
+const float c = 0.2; //0.2
+
+volatile bool isCalibrated;
+int state;
+unsigned int xcenter;
+int dir;
+short steps;
+float period;
+float target_angle;
+unsigned long t0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
+  pinMode(ZERO_PIN, INPUT_PULLUP);
   
-  period = 500000; //500000;
-  dir = 1;
+  attachInterrupt(digitalPinToInterrupt(2), zeroButton, FALLING); //RISING, FALLING, LOW
+  
+  isCalibrated = 0;
+  calibrateAngle();
+  
+  period = 500; //fast ~ 2 to 3
+  target_angle = 90;
   xcenter = width/2;
-  digitalWrite(DIR_PIN, dir);
   
-  Timer1.initialize(period);
-  //Timer1.attachInterrupt(updateCurrentAngle);
-  Timer1.pwm(STEP_PIN, 0.5*1023);
+  digitalWrite(DIR_PIN, dir);
 
   while(Serial.available()>0){ //to clear the buffer
     Serial.read();
   }
+
+  t0 = millis();
   Serial.print("Ready");
 }
 
@@ -50,24 +65,43 @@ int readIntFromBytes() {
 }
 
 void updateCurrentAngle(){ //used as a handler on rising edge of timer to update the steps
-    if(dir==1){
-      steps++;  
-    }
-    else{
-      steps--; 
+    if(state){
+      if(dir==1){
+        steps--;  
+      }
+      else{
+        steps++; 
+      }
     }
 }
 
 void calibrateAngle(){
-    
+  dir = 1;
+  digitalWrite(DIR_PIN, dir);
+  while(!isCalibrated){
+    state = !state;
+    digitalWrite(STEP_PIN, state);
+    delay(2);
+  }
+  steps = 0;
+}
+
+void zeroButton(){
+  if(!isCalibrated){
+    //steps = 0;
+    isCalibrated = true;  
+  }
 }
 
 void loop() {
   while (1) {
-    if(Serial.available() > 2){
+    float current_angle = 0.15*steps;
+    
+    if(Serial.available() > 2){ //use recieved xcenter to calculate target angle
       xcenter = readIntFromBytes();
       int deadcenter = width/2;
-
+      int x_error = (xcenter-deadcenter);
+      
       //set direction
       if(xcenter>deadcenter){
         dir=0; 
@@ -76,38 +110,64 @@ void loop() {
         dir=1;   
       }
 
-      //set period
-      if(xcenter==deadcenter){
-        period=100000; //100000; 
+      //set target_angle
+      float target = current_angle + c*x_error; //TODO: find value of c
+      if(target<180 && target>0){
+        target_angle = target;  
       }
-      else{
-        period = 160000/abs(xcenter-deadcenter);  
-      }
-      if(period<20000){
-        period = 20000;
-      }
-      
-      Serial.print("xcenter: ");
+      /*Serial.print("serial recieved: \n xcenter = ");
       Serial.println(xcenter);
-      Serial.print("period: ");
-      Serial.println(period);
-      Serial.println(dir);
+      Serial.print("target = ");
+      Serial.println(target_angle);
+      Serial.print("current = ");
+      Serial.println(current_angle);
+      Serial.print("x error = ");
+      Serial.println(x_error);
+      */
       
-      Timer1.setPeriod(period);
-      digitalWrite(DIR_PIN, dir);
-      //delay(100);
-      
+    }
+    
+    
+    
+    float error = abs(target_angle-current_angle);
+    if(error<0.15){
+      period = 50;  
     }
     else{
-      
-      
-      
+      if(error>20){
+        period = 1;    
+      }
+      else{
+        period = 1.0/(a*error);
+      }
     }
-    /*Serial.write(highByte(steps));
-    Serial.write(lowByte(steps));
-    //delay(200);
-    Serial.print("steps: ");
-    Serial.println(steps);
+    
+    if(current_angle>target_angle){
+      dir = 1;  
+    }
+    else{
+      dir = 0;  
+    }
+    
+    digitalWrite(STEP_PIN, state);
+    digitalWrite(DIR_PIN, dir);
+    state = !state;
+    updateCurrentAngle();
+    delay(period);
+
+    unsigned long now = millis();
+
+    if(Serial.availableForWrite()>2 && (now-t0)>11.1){ //wait until 90Hz (11.1ms) to send
+      Serial.write(highByte(steps));
+      Serial.write(lowByte(steps));
+      t0 = now;
+    }
+
+    /*Serial.print("error: ");
+    Serial.println(error);
+    Serial.print("period: ");
+    Serial.println(period);
     */
+    
   }
 }
